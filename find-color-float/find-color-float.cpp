9e3d3a1a7877f6b9e201c4489411a087ba3bc93a
gofman5 <gofman5@REVISION-PC>
2022-06-23 20:44:57 +0300

    Добавьте файлы проекта.

---
+++ find-color-float/find-color-float.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(0, "rus");
+
+    {
+        char var = 'Y';
+
+        while (var == 'Y' || 'y')
+        {
+            float a, b = 255, c;
+            cout << (u8"Input RBG color int!\n\n");
+            cin >> a;
+            c = a / b;
+            cout << "\n";
+            cout << u8"Float: \n" << c << endl;
+        
+            cout << "Y/y (продолжить)\n\n" << endl;
+            cin >> var;
+        }
+
+
+        
+    }
+}
+
+
